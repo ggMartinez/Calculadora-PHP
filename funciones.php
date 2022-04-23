785e46df2eab03b85db678cb4643ca324d3328ad
Gonzalo 2 <gon.martinez@icloud.com>
2022-04-23 01:02:28 +0000

    Rompemos todo de nuevo

--- funciones.php
+++ funciones.php
@@ -3,15 +3,8 @@
     function sumar($sumando1,$sumando2){
         echo "$sumando1 + $sumando2 =";
         echo $sumando1 + $sumando2;
-    }
-
-    function multiplicar($factor1,$factor2){
-        echo "$factor1 x $factor2 =";
-        echo $factor1 * $factor2;
-    }
 
     function dividir($dividendo,$divisor){
         echo "$dividendo / $divisor =";
         echo $dividendo / $divisor;
-    }
-
+?>
